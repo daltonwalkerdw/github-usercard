2fddf0508d5c5977985469dc88022d223146d1bb
daltonwalkerdw <53591219+daltonwalkerdw@users.noreply.github.com>
2019-10-10 21:19:47 -0400

    Review

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,6 +3,17 @@
            https://api.github.com/users/<your name>
 */
 
+axios
+.get("https://api.github.com/users/daltonwalkerdw")
+.then((response) => {
+  cards = document.querySelector(".cards")
+  
+  cards.appendChild(returnObj(response))
+ console.log(response)
+})
+.catch((err) => {
+  console.log(err)
+})
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -45,7 +56,55 @@ const followersArray = [];
 </div>
 
 */
+returnObj = (obj) => {
+        // Created Elements
+ divCard = document.createElement("div")
+ img = document.createElement("img")
+ divCardInfo = document.createElement("div")
+ name = document.createElement("h3")
+ username = document.createElement("p")
+ location = document.createElement("p")
+ profile = document.createElement("p")
+ profileLink = document.createElement("a")
+ followers = document.createElement("p")
+ following = document.createElement("p")
+ bio = document.createElement("p")
+       // End Elements
 
+      // add classes
+divCard.classList.add("card")
+divCardInfo.classList.add("card-info")
+name.classList.add("name")
+username.classList.add("username")
+      // end classes
+
+      // add text
+img.src = obj.data.avatar_url;
+name.textContent = obj.data.name   
+username.textContent = obj.data.login
+location.textContent = obj.data.location
+profile.textContent = "Profile:"
+profileLink.textContent = obj.data.html_url
+followers.textContent = obj.data.followers
+following.textContent = obj.data.following
+bio.textContent = obj.data.bio
+      // end text
+      // append
+divCard.appendChild(img)
+divCard.appendChild(divCardInfo)    
+divCardInfo.appendChild(name) 
+divCardInfo.appendChild(username)   
+divCardInfo.appendChild(location)  
+divCardInfo.appendChild(profile)
+profile.appendChild(profileLink)  
+divCardInfo.appendChild(followers)
+divCardInfo.appendChild(following)   
+divCardInfo.appendChild(bio)   
+      // end append
+    console.log(divCard)  
+
+    return divCard  
+}
 /* List of LS Instructors Github username's: 
   tetondan
   dustinmyers
